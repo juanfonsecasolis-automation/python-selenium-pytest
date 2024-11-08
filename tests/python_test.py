40ee861066a3ecfcf4358dba15c071d04bbec907
Juan Fonseca <juan.fonsecasolis@outlook.com>
2024-11-08 16:03:43 -0600

    Removed python language tests.

--- tests/python_test.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from selenium.webdriver.common.by import By
-
-class TestPython:
-
-    @pytest.mark.python
-    def test_packing_argument(self):
-        var = By.XPATH, '//*'
-        assert type(var).__name__=='tuple'

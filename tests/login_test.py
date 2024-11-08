333d5e34cb38241aa24c73fc7a1735ec640d5651
Juan Fonseca <juan.fonsecasolis@outlook.com>
2024-11-08 15:41:59 -0600

    Added test parametrization.

--- tests/login_test.py
+++ tests/login_test.py
@@ -6,11 +6,16 @@ class TestLogin:
     @pytest.mark.positiveTests
     def test_positive_login(self, driver):
         loginPage = LoginPage(driver)
-        loginPage.login("standard_user", "secret_sauce")
+        loginPage.login('standard_user', 'secret_sauce')
         assert "Products" in driver.page_source
 
     @pytest.mark.negativeTests
-    def test_negative_login(self, driver):
+    @pytest.mark.parametrize(
+        'username,password', 
+        [
+            ('bad_user', 'secret_sauce'), 
+            ('standard_user', 'bad_sauce')])
+    def test_negative_login(self, username, password, driver):
         loginPage = LoginPage(driver)
-        loginPage.login("standard_user", "ABC123")
+        loginPage.login(username, password)
         assert "Products" not in driver.page_source
